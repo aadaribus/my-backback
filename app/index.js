d097f6265c522543cd260b48ec22f05f0b252215
aadaribusweb <aadaribusweb@gmail>
2026-05-09 22:34:51 -0600

    git add . git commit -m "Corrigiendo variables de entorno para Render"

--- app/index.js
+++ app/index.js
@@ -6,6 +6,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Cargar variables de entorno
+dotenv.config();
+
 // Inicializar Supabase después de cargar dotenv
 import { initSupabase } from "./config/supabase.js";
 initSupabase();
